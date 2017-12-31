0dcb9874bf1f68b589780b286c7ee5e1e071efda
Curtis <cslack49@gmail.com>
2017-12-30 19:52:40 -0700

    Read and write EXIF data, errors on write.

--- exif_editor.py
+++ exif_editor.py
@@ -1,2 +1,14 @@
-print "EXIF Editor"
-print "Test line"
+from PIL import Image
+import piexif
+
+filename = "Test_Image.jpg"
+
+image = Image.open(filename)
+exif_dict = piexif.load(image.info["exif"])
+
+print exif_dict
+
+exif_dict["0th"]["270"] = "Revised Test Image Description"
+exif_bytes = piexif.dump(exif_dict)
+
+image.save("Revised Test Image.jpg", "jpeg", exif = exif_bytes)
