cad3a0c88a509a9f8dcb8589fba3dd144a0263a8
Kent Slack <kent.slack@gmail.com>
2018-01-11 20:54:35 -0700

    Revised the code so the tags are now successfully updated. Modifies both the Description tag and the Comments tag.

--- exif_editor.py
+++ exif_editor.py
@@ -8,7 +8,13 @@ exif_dict = piexif.load(image.info["exif"])
 
 print exif_dict
 
-exif_dict["0th"]["270"] = "Revised Test Image Description"
+description = raw_input("Image Description = ")
+exif_dict['0th'][270] = description
 exif_bytes = piexif.dump(exif_dict)
 
+# Set User Comment Exif Tag 37510
+exif_dict['Exif'][37510] = description
+
 image.save("Revised Test Image.jpg", "jpeg", exif = exif_bytes)
+
+image.close()
